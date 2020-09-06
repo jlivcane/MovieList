52f6dd167bd649da4e491f4a9fc82f54f63ce0b0
jlivcane <kalibrik@inbox.lv>
2020-09-06 22:13:53 +0300

    non working build

---
+++ MovieList/Controller/MovieTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieTableViewCell.swift
+//  MovieList
+//
+//  Created by jekaterina.livcane on 06/09/2020.
+//  Copyright © 2020 jekaterina.livcane. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var filmImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var directorLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MovieList/Controller/ViewController.swift
+++ MovieList/Controller/ViewController.swift
@@ -9,22 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var editButtonTapped: UIBarButtonItem!
+    
+    var films = Film.createFilm()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureCell(cell: MovieTableViewCell, for indexPath: IndexPath){
+        let film = films[indexPath.row]
+        cell.filmImageView.image = UIImage(named: film.poster)
+        cell.titleLabel.text = film.movieName
+        cell.directorLabel.text = film.director
+        
     }
-    */
-
+    
 }
+
+extension ViewController:UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return films.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+            as? MovieTableViewCell else {
+                return UITableViewCell()
+        }
+        
+        
+        configureCell(cell: cell, for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let alert = UIAlertController(title: "Are you sure you want to delete?", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: {(_) in self.films.remove(at: indexPath.row)
+                UIView.transition(with: tableView, duration: 1.0, options: .transitionCrossDissolve, animations: {
+                    self.tableView.reloadData()
+                }, completion: nil)
+            }))
+            self.present(alert, animated: true)
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+     func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let currentMovie = films.remove(at: fromIndexPath.row)
+        films.insert(currentMovie, at: to.row)
+    }
+}
+
